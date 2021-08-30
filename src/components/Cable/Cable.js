7cf80b04a9e837c10c72992b1370bfdae21b9ded
Andrey0295 <andreevicandrej00@gmail.com>
2021-08-30 14:41:28 +0300

    initial commit

---
+++ src/components/Cable/Cable.js
@@ -0,0 +1,23 @@
+import React, { Fragment } from "react";
+import { ActionCableConsumer } from "react-actioncable-provider";
+
+const Cable = ({ conversations, handleReceivedMessage }) => {
+  return (
+    <Fragment>
+      {conversations.map((conversation) => {
+        return (
+          <ActionCableConsumer
+            key={conversation.id}
+            channel={{
+              channel: "MessagesChannel",
+              conversation: conversation.id,
+            }}
+            onReceived={handleReceivedMessage}
+          ></ActionCableConsumer>
+        );
+      })}
+    </Fragment>
+  );
+};
+
+export default Cable;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ActionCableProvider } from "react-actioncable-provider";
+import "./index.css";
+import App from "./App";
+import Container from "./components/Container/Container";
+import { API_WS_ROOT } from "./constants/constants";
+import ActionCable from "actioncable";
+
+import reportWebVitals from "./reportWebVitals";
+
+const cable = ActionCable.createConsumer(API_WS_ROOT);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <ActionCableProvider cable={cable}>
+    <Container>
+      <App />
+    </Container>
+  </ActionCableProvider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
